0f26573293ca8e80a1ecde6a38542346a92023b0
yanjinfeng101 <58726560+yanjinfeng101@users.noreply.github.com>
2020-10-16 14:55:34 +0800

    Update attention.py

--- layers/attention.py
+++ layers/attention.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+#注意力机制
 class Attention(nn.Module):
     def __init__(self, embed_dim, hidden_dim=None, out_dim=None, n_head=1, score_function='dot_product', dropout=0):
         ''' Attention Mechanism
@@ -24,20 +24,20 @@ class Attention(nn.Module):
             hidden_dim = embed_dim // n_head
         if out_dim is None:
             out_dim = embed_dim
-        self.embed_dim = embed_dim
+        self.embed_dim = embed_dim#变量前面加self，那么在任何实例方法就都可以访问这个变量了
         self.hidden_dim = hidden_dim
         self.n_head = n_head
         self.score_function = score_function
-        self.w_k = nn.Linear(embed_dim, n_head * hidden_dim)
-        self.w_q = nn.Linear(embed_dim, n_head * hidden_dim)
-        self.proj = nn.Linear(n_head * hidden_dim, out_dim)
+        self.w_k = nn.Linear(embed_dim, n_head * hidden_dim)#key线性变换,nlp中key= value
+        self.w_q = nn.Linear(embed_dim, n_head * hidden_dim)#query线性变换
+        self.proj = nn.Linear(n_head * hidden_dim, out_dim)#权重矩阵的size正好是我们输入的行列转置
         self.dropout = nn.Dropout(dropout)
         if score_function == 'mlp':
             self.weight = nn.Parameter(torch.Tensor(hidden_dim*2))
         elif self.score_function == 'bi_linear':
             self.weight = nn.Parameter(torch.Tensor(hidden_dim, hidden_dim))
         else:  # dot_product / scaled_dot_product
-            self.register_parameter('weight', None)
+            self.register_parameter('weight', None)#将一个不可训练的类型Tensor转换成可以训练的类型parameter，并将这个parameter绑定到这个module.成为了模型中可以根据训练进行变化的参数。
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -50,7 +50,7 @@ class Attention(nn.Module):
             q = torch.unsqueeze(q, dim=1)
         if len(k.shape) == 2:  # k_len missing
             k = torch.unsqueeze(k, dim=1)
-        mb_size = k.shape[0]  # ?
+        mb_size = k.shape[0]  # ?应该是batch_size吧
         k_len = k.shape[1]
         q_len = q.shape[1]
         # k: (?, k_len, embed_dim,)
@@ -59,10 +59,10 @@ class Attention(nn.Module):
         # qx: (n_head*?, q_len, hidden_dim)
         # score: (n_head*?, q_len, k_len,)
         # output: (?, q_len, out_dim,)
-        kx = self.w_k(k).view(mb_size, k_len, self.n_head, self.hidden_dim)
-        kx = kx.permute(2, 0, 1, 3).contiguous().view(-1, k_len, self.hidden_dim)
+        kx = self.w_k(k).view(mb_size, k_len, self.n_head, self.hidden_dim)#(?, k_len,n_head, hidden_dim)
+        kx = kx.permute(2, 0, 1, 3).contiguous().view(-1, k_len, self.hidden_dim)# kx: (n_head*?, k_len, hidden_dim)
         qx = self.w_q(q).view(mb_size, q_len, self.n_head, self.hidden_dim)
-        qx = qx.permute(2, 0, 1, 3).contiguous().view(-1, q_len, self.hidden_dim)
+        qx = qx.permute(2, 0, 1, 3).contiguous().view(-1, q_len, self.hidden_dim)# qx: (n_head*?, q_len, hidden_dim)
         if self.score_function == 'dot_product':
             kt = kx.permute(0, 2, 1)
             score = torch.bmm(qx, kt)
@@ -71,15 +71,18 @@ class Attention(nn.Module):
             qkt = torch.bmm(qx, kt)
             score = torch.div(qkt, math.sqrt(self.hidden_dim))
         elif self.score_function == 'mlp':
-            kxx = torch.unsqueeze(kx, dim=1).expand(-1, q_len, -1, -1)
+            kxx = torch.unsqueeze(kx, dim=1).expand(-1, q_len, -1, -1)#expand（）函数的功能是用来扩展张量中某维数据的尺寸，只能将size=1的维度扩展到更大的尺寸
+            #kxx: (n_head*?, q_len,k_len, hidden_dim)
             qxx = torch.unsqueeze(qx, dim=2).expand(-1, -1, k_len, -1)
+            #qxx: (n_head*?, q_len, k_len,hidden_dim)
             kq = torch.cat((kxx, qxx), dim=-1)  # (n_head*?, q_len, k_len, hidden_dim*2)
             # kq = torch.unsqueeze(kx, dim=1) + torch.unsqueeze(qx, dim=2)
-            score = F.tanh(torch.matmul(kq, self.weight))
-        elif self.score_function == 'bi_linear':
-            qw = torch.matmul(qx, self.weight)
-            kt = kx.permute(0, 2, 1)
-            score = torch.bmm(qw, kt)
+            score = F.tanh(torch.matmul(kq, self.weight))#hidden_dim*2，得到(n_head*?, q_len, k_len)
+        elif self.score_function == 'bi_linear':#双线性模型
+            qw = torch.matmul(qx, self.weight)#(n_head*?, q_len, hidden_dim),(hidden_dim, hidden_dim)
+            #(n_head*?, q_len, hidden_dim)
+            kt = kx.permute(0, 2, 1)#kx: (n_head*?, k_len, hidden_dim),(n_head*?, hidden_dim,k_len)
+            score = torch.bmm(qw, kt)#(n_head*?, q_len, k_len)
         else:
             raise RuntimeError('invalid score_function')
         score = F.softmax(score, dim=-1)
